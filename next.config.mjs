515652d3b209eaaf733914d4757b3b7e41022be2
shankar samidala <shankar@kalpas.in>
2024-07-14 10:02:44 +0530

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      loader: 'imgix',  // Using imgix as a loader
-      path: 'https://idealmart.ca/',  // Set your own domain or path here
-    },
+    // output: 'export',
+    
   };
-  
-  module.exports = nextConfig;
+export default nextConfig;
   
